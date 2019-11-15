3594b8e76d5d147e935b4a0831e325e3b04c5238
oredola <oredolaolamide@gmail.com>
2019-11-15 12:03:01 +0100

    CharacterCard component built

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  
+  return (
+    <div key={character.id}>
+      <img src={character.image} />
+      <h1>{character.name}</h1>
+      <p>Status: {character.status}</p>
+      <p>Location: {character.location.name}</p>
+      <p>Location: {character.origin.name}</p>
+      <p>Number of Episodes: `${character.episode.length} episodes`</p>
+    </div>
+  );
 }
