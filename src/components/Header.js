4a1a728bfef9e0a8685f185fc5312496ba6e3d12
oredola <oredolaolamide@gmail.com>
2019-11-15 17:04:06 +0100

    Link to characters and search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import SearchForm from './SearchForm';
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to="/characters" />
+      <SearchForm />
     </header>
   );
 }
